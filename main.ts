e93bb8822de6739b315c0fe0288b973a845b159a
KOUSER BIBI <mechezaz@gmail.com>
2026-04-25 22:17:36 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.coolRadial, 1000)
-    scene.cameraShake(89, 1000)
+    scene.cameraShake(89, 11000)
     info.changeLifeBy(-1)
 })
 let bogey: Sprite = null
